2ff0199264a6de197e27bc64435d0dd8d3c0ac0d
elementalcharge <german.boso@gmail.com>
2018-06-10 20:38:53 -0300

    agregado de test con junit 5

---
+++ src/test/TestCarta.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCarta {
+
+	@Test
+	void testCartaEnModoDeAtaque() {
+		fail("Not yet implemented");
+	}
+
+}
