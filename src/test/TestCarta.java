3283cfba7cdd877637369021ef69c2fc1041075c
elementalcharge <german.boso@gmail.com>
2018-06-12 00:17:49 -0300

    agregado ventana basica aun no se como manejar el resto

--- src/test/TestCarta.java
+++ src/test/TestCarta.java
@@ -1,4 +1,13 @@
 package test;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,7 +17,7 @@ class TestCarta {
 
 	@Test
 	void testCartaEnModoDeAtaque() {
-		fail("Not yet implemented");
+		
 	}
 
 }
