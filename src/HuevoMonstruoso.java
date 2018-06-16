7b66c1d375241b9414619363cadc279437ae2fbd
German Daniel Boso <german.boso@gmail.com>
2018-06-16 17:37:13 -0300

    Update HuevoMonstruoso.java
    agregamos modo ataque modo defensa y invocacion

--- src/HuevoMonstruoso.java
+++ src/HuevoMonstruoso.java
@@ -1,8 +1,31 @@
 public class HuevoMonstruoso {
-    public Object getUbicacion() {
+    private Modo modo;
+
+	public Object getUbicacion() {
         return new Cementerio();
     }
-
+	private void cambiarEstado(Modo m) {
+		this.modo= m;
+	}
     public void atacar(AgujeroNegro agujeroNegro) {
     }
+
+	public void invocar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void modoDeAtaque() {
+		// TODO Auto-generated method stub
+		 cambiarEstado(new ModoDeAtaque());
+	}
+
+	public Object modo() {
+		// TODO Auto-generated method stub
+		return modo;
+	}
+
+	public void modoDeDefensa() {
+		cambiarEstado(new ModoDeDefensa());
+	}
 }
